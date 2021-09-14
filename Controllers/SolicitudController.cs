60b45ba67a93b6fab80a067e5e744440a9c3f0b7
Plopez11 <Pedrolopeze11@gmail.com>
2021-09-14 18:15:35 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/SolicitudController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebHogwarts_bl.Models;
+using WebHogwarts_bl.Data;
+
+namespace WebApiRest_Hogwarts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SolicitudController : ControllerBase
+    {
+        private readonly Hogwarts_Context _context;
+
+        public SolicitudController(Hogwarts_Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Solicitud
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Solicitud>>> GetSolicitud()
+        {
+            return await _context.Solicitud.ToListAsync();
+        }
+
+        // GET: api/Solicitud/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Solicitud>> GetSolicitud(int id)
+        {
+            var solicitud = await _context.Solicitud.FindAsync(id);
+
+            if (solicitud == null)
+            {
+                return NotFound();
+            }
+
+            return solicitud;
+        }
+
+        // PUT: api/Solicitud/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSolicitud(int id, Solicitud solicitud)
+        {
+            if (id != solicitud.cedula)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(solicitud).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SolicitudExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Solicitud
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Solicitud>> PostSolicitud(Solicitud solicitud)
+        {
+            _context.Solicitud.Add(solicitud);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSolicitud", new { id = solicitud.cedula }, solicitud);
+        }
+
+        // DELETE: api/Solicitud/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Solicitud>> DeleteSolicitud(int id)
+        {
+            var solicitud = await _context.Solicitud.FindAsync(id);
+            if (solicitud == null)
+            {
+                return NotFound();
+            }
+
+            _context.Solicitud.Remove(solicitud);
+            await _context.SaveChangesAsync();
+
+            return solicitud;
+        }
+
+        private bool SolicitudExists(int id)
+        {
+            return _context.Solicitud.Any(e => e.cedula == id);
+        }
+    }
+}
