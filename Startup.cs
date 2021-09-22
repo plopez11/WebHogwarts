8a9e2d3b4bd1cc481cdd8244e2a8738199c0951d
Plopez11 <Pedrolopeze11@gmail.com>
2021-09-22 21:32:46 +0200

    Primer commit

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using WebHogwarts_bl.Data;
+
 
 namespace WebApiRest_Hogwarts
 {
@@ -13,28 +16,48 @@ namespace WebApiRest_Hogwarts
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            
         }
 
+
         public IConfiguration Configuration { get; }
+        internal static MapperConfiguration MapperConfiguration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-
             services.AddDbContext<Hogwarts_Context>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("Hogwarts_Context")));
+
+
+            services.AddControllers();
+
+
+            services.AddSwaggerGen();
+
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger { Title = "WebApiRest_Hogwart", Version = "v1" });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("./v1/swagger.json", "My API V1"); //originally "./swagger/v1/swagger.json"
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseHttpsRedirection();
+
 
             app.UseRouting();
 
@@ -44,6 +67,8 @@ namespace WebApiRest_Hogwarts
             {
                 endpoints.MapControllers();
             });
+
+
         }
     }
 }
