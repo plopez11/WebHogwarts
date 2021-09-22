244cc1e41a77183cb38649f3b9400f2b42e6b28e
Plopez11 <Pedrolopeze11@gmail.com>
2021-09-22 21:59:46 +0200

    Se agrega WebHogwarts_bl

---
+++ WebHogwarts.bl/Data/Hogwarts_Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebHogwarts_bl.Models;
+using WebHogwarts_bl.Data;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace WebHogwarts_bl.Data
+{
+    public class Hogwarts_Context : DbContext
+    {
+        public Hogwarts_Context(DbContextOptions<Hogwarts_Context> options)
+            : base(options)
+        {
+                
+        }
+       public DbSet<Solicitud> Solicitud { get; set; }
+
+        //public static Hogwarts_Context Create()
+        //{
+        //    return new Hogwarts_Context();
+        //}
+
+    }
+}
---
+++ WebHogwarts.bl/Models/Solicitud.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebHogwarts_bl.Models
+{
+    [Table("solicitud", Schema = "dbo")]
+    public class Solicitud
+    {      
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int cedula { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public Int16 edad { get; set; }
+        public string casa_afiliada { get; set; }
+    }
+}
